ca0136763fe39b84204c684ae7ecee8fe6d02919
kimsj0970 <53886275+kimsj0970@users.noreply.github.com>
2025-01-01 20:41:43 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 752 ms, Memory: 137304 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
@@ -53,17 +56,17 @@ public class Main {
         return -1;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        n = sc.nextInt();
-        m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String [] s = br.readLine().split(" ");
+        n = Integer.parseInt(s[0]);
+        m = Integer.parseInt(s[1]);
 
         arr = new int[n][m];
         visit = new int[n][m][2];
 
         for (int i = 0; i < n; i++) {
-            String line = sc.next();
+            String line = br.readLine();
             for (int j = 0; j < m; j++) {
                 arr[i][j] = line.charAt(j) - '0';
             }
