27e6fe8f2c5e2921a79a957e0f7f93b232726afc
kimsj0970 <53886275+kimsj0970@users.noreply.github.com>
2025-01-28 22:22:00 +0900

    [Silver II] Title: 로또, Time: 224 ms, Memory: 16248 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -3,8 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    static void back(int dep, int start, int k, int[] arr, int[] selete) {
+    static int k;
+    static int selete[];
+    static int arr[];
+    
+    static void back(int dep, int start) {
         if (dep == 6) {
             for (int i = 0; i < 6; i++) {
                 System.out.print(selete[i] + " ");
@@ -15,7 +18,7 @@ public class Main {
 
         for (int i = start; i < k; i++) {
             selete[dep] = arr[i];
-            back(dep + 1, i + 1, k, arr, selete);
+            back(dep + 1, i + 1);
         }
     }
 
@@ -24,18 +27,17 @@ public class Main {
 
         while (true) {
             String[] s = br.readLine().split(" ");
-            int k = Integer.parseInt(s[0]);
+            k = Integer.parseInt(s[0]);
 
-            if (k == 0) break; 
-
-            int[] arr = new int[k];
+            if (k == 0) break;
+            selete = new int[6];
+            arr = new int[k];
             for (int i = 0; i < k; i++) {
                 arr[i] = Integer.parseInt(s[i + 1]);
             }
-
-            int[] selete = new int[6]; 
-            back(0, 0, k, arr, selete);
-            System.out.println(); 
+            
+            back(0, 0);
+            System.out.println();
         }
     }
 }
